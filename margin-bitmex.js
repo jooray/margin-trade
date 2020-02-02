a397d3735510cec520a60bfed07a17e054e327db
Tomas Kanocz <tomas.kanocz@cnl.sk>
2020-02-02 17:35:38 +0100

    Add exchange abstraction and deribit implementations

--- margin-bitmex.js
+++ margin-bitmex.js
@@ -10,35 +10,33 @@ if (process.env.TESTNET === 'true') {
   exchange.urls.api = exchange.urls.test
 }
 
-balancePromise = exchange.fetchBalance()
-positionsPromise = exchange.privateGetPosition({
-  filter: {
-    "isOpen": true,
-    "symbol": "XBTUSD"
-  }
-})
-Promise.all([balancePromise, positionsPromise]).then( (values) => {
-  balance = values[0]
-  positions = values[1]
-  console.error('Balance: ' + balance.total.BTC)
-  if (positions.length > 0) {
-    liquidationPrice = positions[0].liquidationPrice
-    lastPrice = positions[0].lastPrice
-    liquidationPercentage = liquidationPrice / lastPrice
-    console.error('Last price: ' + lastPrice)
-    console.error('Liquidation price: ' + liquidationPrice)
-    console.error('Margin of safety: ' + (liquidationPercentage*100).toFixed(2) + '%')
-    if ((argv.w) && (argv.w > liquidationPercentage)) {
-      console.log("Our liquidation margin is below threshold!")
-      exchange.privateGetUserDepositAddress({currency: 'XBt'}).then( (response) => {
-        console.log("Deposit to: " + response)
-        process.exit(1)
-      })
+let [balance, positions ] = await Promise.all([
+  exchange.fetchBalance(),
+  exchange.privateGetPosition({
+    filter: {
+      isOpen: true,
+      symbol: "XBTUSD"
     }
-  } else {
-    console.error('No positions open')
+  })
+])
+
+console.error('Balance: ' + balance.total.BTC)
+if (positions.length > 0) {
+  liquidationPrice = positions[0].liquidationPrice
+  lastPrice = positions[0].lastPrice
+  liquidationPercentage = liquidationPrice / lastPrice
+  console.error('Last price: ' + lastPrice)
+  console.error('Liquidation price: ' + liquidationPrice)
+  console.error('Margin of safety: ' + (liquidationPercentage*100).toFixed(2) + '%')
+  if ((argv.w) && (argv.w > liquidationPercentage)) {
+    console.log("Our liquidation margin is below threshold!")
+    exchange.privateGetUserDepositAddress({currency: 'XBt'}).then( (response) => {
+      console.log("Deposit to: " + response)
+      process.exit(1)
+    })
   }
+} else {
+  console.error('No positions open')
 }
-)
 
 })()
--- trade-bitmex.js
+++ trade-bitmex.js
@@ -2,9 +2,15 @@
 require('dotenv').config()
 var ccxt = require ('ccxt')
 
-var config = {
- 'apiKey': process.env.TRADEAPIKEY,
- 'secret': process.env.TRADEAPISECRET,
+var configs = {
+  bitmex: {
+    apiKey: process.env.APIKEY,
+    secret: process.env.APISECRET,
+  },
+  deribit: {
+    apiKey: process.env.DERIBITAPIKEY,
+    secret: process.env.DERIBITAPISECRET
+  }
 }
 //getExchange :: String -> StrMap -> Object
 let getExchange = exchange => config => {
@@ -15,10 +21,35 @@ let getExchange = exchange => config => {
   return res
 }
 
-async function market (argv) {
+// console.log(getExchange('deribit')({}))
+// const deribit = getExchange('deribit')({
+//   apiKey: process.env.DERIBITAPIKEY,
+//   secret: process.env.DERIBITAPISECRET
+// })
+// let info = await deribit.privateGetPositions()
+// console.log(info)
+// process.exit(0)
+const exchanges = ['bitmex', 'deribit']
+const exchangeAbstraction = method => argv => {
+  let methods = {
+    bitmex: {
+      market: marketBitmex,
+      position: positionBitmex,
+      fundingRate: fundingRateBitmex
+    },
+    deribit: {
+      market: marketDeribit,
+      position: positionDeribit,
+      fundingRate: fundingRateDeribit
+    }
+  }
+  return methods[argv.exchange][method](argv)
+}
+
+async function marketBitmex (argv) {
   console.log('Submitting a ' + argv.side + ' order for ' + argv.amount + ' on ' + argv.symbol)
 
-  let exchange = getExchange('bitmex')(config)
+  let exchange = getExchange('bitmex')(configs.bitmex)
 
   let orderResult = await exchange.privatePostOrder({
     symbol: argv.symbol,
@@ -33,9 +64,27 @@ async function market (argv) {
   console.log('Order ID: ' + ordId)
 }
 
-async function position(argv) {
+async function marketDeribit (argv) {
+  console.log("uninplemented");
+}
 
-  let exchange = getExchange('bitmex')(config)
+function printPosition({currency, quoteCurrency, positionAmount, price, entry, value}) {
+  let humanText = `Position:\t ${positionAmount} ${quoteCurrency}\n` +
+    `Current price:\t ${price} ${quoteCurrency}\n` +
+    `Entry price:\t ${+entry.toFixed(2)} ${quoteCurrency}\n` +
+    `Current value:\t ${+(value/100000000).toFixed(8)} ${currency}`
+  let jsonText = JSON.stringify({
+    positionAmount,
+    price,
+    entry: +entry.toFixed(2),
+    value // has to be in satoshi
+  })
+  console.log(process.env.JSON_OUTPUT === 'true' ? jsonText : humanText)
+}
+
+async function positionBitmex(argv) {
+
+  let exchange = getExchange('bitmex')(configs.bitmex)
   let positions = await exchange.privateGetPosition({
     filter: {
       isOpen: true,
@@ -43,22 +92,39 @@ async function position(argv) {
     }
   })
   for (const position of positions) {
-    let humanText = `Position:\t ${position.currentQty} ${position.quoteCurrency}\n` +
-      `Current price:\t ${position.lastPrice} ${position.quoteCurrency}\n` +
-      `Entry price:\t ${+position.avgEntryPrice.toFixed(2)} ${position.quoteCurrency}\n` +
-      `Current value:\t ${position.lastValue/100000000} ${position.currency}`
-    let jsonText = JSON.stringify({
-      position: position.currentQty,
+    printPosition({
+      currency: position.currency,
+      quoteCurrency: position.quoteCurrency,
+      positionAmount: position.currentQty,
       price: position.lastPrice,
-      entry: +position.avgEntryPrice.toFixed(2),
+      entry: position.avgEntryPrice,
       value: position.lastValue
     })
-    console.log(process.env.JSON_OUTPUT === 'true' ? jsonText : humanText)
   }
 }
 
-async function fundingRate (argv) {
+async function positionDeribit (argv) {
+  let exchange = getExchange ('deribit') (configs.deribit)
+  let positions = await exchange.privateGetPositions()
+  for (const position of positions.result) {
+    printPosition({
+      currency: position.currency,
+      quoteCurrency: 'USD',
+      positionAmount: position.amount,
+      price: position.markPrice,
+      entry: position.averagePrice,
+      value: Math.ceil(position.sizeBtc * 100000000)
+    })
+  }
+}
 
+function printFundingRate(symbol, rate) {
+  console.log(`Instrument ${symbol} funding rate ` +
+  `${rate} which is approx ` +
+  `${+(rate*3*365*100).toFixed(2)} % p.a.`)
+}
+
+async function fundingRateBitmex (argv) {
   let exchange = getExchange('bitmex')({})
   let instruments = await exchange.publicGetInstrument({
     filter: {
@@ -67,17 +133,37 @@ async function fundingRate (argv) {
     symbol: argv.symbol
   })
   for (const instrument of instruments) {
-      console.log('Instrument ' + instrument.symbol + ' funding rate ' +
-        instrument.fundingRate + ' which is approx ' +
-        +(instrument.fundingRate*3*365*100).toFixed(2) + '% p.a.')
+    printFundingRate(instrument.symbol, instrument.fundingRate)
   }
 }
+
+async function fundingRateDeribit (argv) {
+  let rp = require('request-promise')
+  let fundingRate = await rp({
+    uri: `https://${process.env.TESTNET === 'true' ? 'test.' : ''}deribit.com/api/v2/public/get_funding_chart_data`,
+    qs: {
+      instrument_name: 'BTC-PERPETUAL',
+      length: '8h'
+    },
+    json: true
+  })
+  fundingRate = fundingRate.result.current_interest
+  printFundingRate('BTC-PERPETUAL', fundingRate)
+}
+
 
 require('yargs')
   .scriptName("trade-bitmex")
   .usage('$0 <cmd> [args]')
-  .command('market side amount [symbol]', 'submit a market order', (yargs) => {
-    yargs.positional('side', {
+  .command('market [exchange] side amount [symbol]', 'submit a market order', (yargs) => {
+    yargs
+    .positional('exchange', {
+      type: 'string',
+      describe: 'exchange to use',
+      choices: exchanges,
+      default: 'bitmex'
+    })
+    .positional('side', {
       type: 'string',
       describe: 'either buy or sell',
       choices: ['buy', 'sell']
@@ -89,21 +175,35 @@ require('yargs')
       describe: 'which market to post order to, default XBTUSD',
       default: 'XBTUSD'
     })
-  }, market)
-  .command('position [symbol]', 'show current position', (yargs) => {
-    yargs.positional('symbol', {
+  }, exchangeAbstraction('market'))
+  .command('position [exchange] [symbol]', 'show current position', (yargs) => {
+    yargs
+    .positional('exchange', {
+      type: 'string',
+      describe: 'exchange to use',
+      choices: exchanges,
+      default: 'bitmex'
+    })
+    .positional('symbol', {
       type: 'string',
       describe: 'which market to show position in, default XBTUSD',
       default: 'XBTUSD'
     })
-  }, position)
-  .command('fundingrate [symbol]', 'show funding rate', (yargs) => {
-    yargs.positional('symbol', {
+  }, exchangeAbstraction('position'))
+  .command('fundingrate [exchange] [symbol]', 'show funding rate', (yargs) => {
+    yargs
+    .positional('exchange', {
+      type: 'string',
+      describe: 'exchange to use',
+      choices: exchanges,
+      default: 'bitmex'
+    })
+    .positional('symbol', {
       type: 'string',
       describe: 'which market to show funding rate of, default XBTUSD',
       default: 'XBTUSD'
     })
-  }, fundingRate)
+  }, exchangeAbstraction('fundingRate'))
   .help()
   .argv
 
